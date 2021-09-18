d24a48feffd50b62b9ef9807085edb1859c47f2e
Biosaur <biosaur99@gmail.com>
2021-09-18 06:38:12 -0400

    Crusty
    it's crusty now

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
   render() {
 
     return (
-      <div>
+      <div class="container">
         <div class="row">
           <div class="col-md-6">
             <input type="file" onChange={this.onSong1Change} />
@@ -96,6 +96,49 @@ class App extends Component {
           Upload!
         </button>
         {this.fileData()}
+
+        <div class="player">
+
+
+          <div class="details">
+            <div class="now-playing">PLAYING x OF y</div>
+            <div class="track-art"></div>
+            <div class="track-name">Track Name</div>
+            <div class="track-artist">Track Artist</div>
+          </div>
+
+
+          <div class="buttons">
+            <div class="prev-track" onclick="prevTrack()">
+              <i class="fa fa-step-backward fa-2x"></i>
+            </div>
+            <div class="playpause-track" onclick="playpauseTrack()">
+              <i class="fa fa-play-circle fa-5x"></i>
+            </div>
+            <div class="next-track" onclick="nextTrack()">
+              <i class="fa fa-step-forward fa-2x"></i>
+            </div>
+          </div>
+
+
+          <div class="slider_container">
+            <div class="current-time">00:00</div>
+            <input type="range" min="1" max="100"
+              value="0" class="seek_slider" onChange="seekTo()"/>
+            <div class ="total-duration">00: 00</div>
+          </div>
+
+
+          <div class="slider_container">
+            <i class="fa fa-volume-down"></i>
+            <input type="range" min="1" max="100"
+              value="99" class="volume_slider" onChange="setVolume()"/>
+            <i class ="fa fa-volume-up"></i>
+          </div>
+        </div>
+
+
+        <script src="main.js"></script>
       </div>
     );
   }
